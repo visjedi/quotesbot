1648a364df3f71d0fba3c62a255773e234095761
visjedi <visjedi@visjedi.com>
2017-07-28 10:13:11 +0200

    first commit

--- quotesbot/pipelines.py
+++ quotesbot/pipelines.py
@@ -5,7 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from datetime import datetime
 
 class QuotesbotPipeline(object):
-    def process_item(self, item, spider):
-        return item
+	
+	def process_item(self, item, spider):
+		print('Processed item of rank {}'.format(item['rank']))
+		item['date_added'] = datetime.now()
+		return item
